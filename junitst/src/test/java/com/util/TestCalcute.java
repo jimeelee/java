4181c6a6aa53a5ad0b1b94cea942ceae80c748b4
jimeelee <lifu1983308@163.com>
2016-08-27 19:05:55 +0800

    create

---
+++ junitst/src/test/java/com/util/TestCalcute.java
@@ -0,0 +1,18 @@
+package com.util;
+
+import org.junit.Assert;
+import org.junit.Before;
+
+public class TestCalcute {
+	Calcute cal;
+	
+	@Before
+	public void setUp(){
+		cal = new Calcute();
+	}
+	
+	@Test
+	public void testAdd(){
+		//assertArrayEquals.
+	}
+}
